bff8d93deb8e2a16345bee81efacf640160df073
itgawri <136715144+itgawri@users.noreply.github.com>
2024-01-24 13:19:31 +0100

    Grades.py

--- Grades.py
+++ Grades.py
@@ -17,26 +17,3 @@ print(f"Sam participated in sporting activities: {sporting_activities}")
 print(f"Sam's total percentage as an integer is {total_percentage}%")
 print(f"Sam passed to the next semester: {did_student_pass}")
 print(f"Sam's average is.: {total_medium}")
-
-# PL-------------------------------------------------------------------------------------
-
-imie_studenta = "Sam"
-ocena_matematyka = 88
-ocena_przyroda = 75
-ocena_angielski = 90
-ocena_geologia = 69
-ocena_polski = 91
-ocena_informatyka = 99
-suma_ocen = ocena_matematyka + ocena_przyroda + ocena_angielski + ocena_geologia + ocena_polski + ocena_informatyka
-maksymalna_ocena = 600
-suma_waga = suma_ocen / 0.6
-procent_ocen = suma_ocen / maksymalna_ocena * 100
-print(f"Procentowa ocena Sama wynosi {procent_ocen}%")
-procent_ocen = int(procent_ocen)
-czy_student_zdal = procent_ocen >= 50
-uczestnictwo_w_aktywnosciach_sportowych = bool(0)
-print(f"Sam brał udział w zajęciach sportowych: {uczestnictwo_w_aktywnosciach_sportowych}")
-print(f"Procentowa ocena Sama jako liczba całkowita wynosi {procent_ocen}%")
-print(f"Sam przeszedł do kolejnego semestru: {czy_student_zdal}")
-print(f"Średnia ważona Sama wynosi: {suma_waga}")
-
